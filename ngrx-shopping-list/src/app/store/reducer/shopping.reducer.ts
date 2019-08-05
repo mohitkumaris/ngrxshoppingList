baf58ba0be46822ee0b08cf0c474f8748ebb7f0b
Mohit Kumar <mohit.kumar2@soprasteria.com>
2019-08-05 11:37:23 +0530

    modifed reducer

--- ngrx-shopping-list/src/app/store/reducer/shopping.reducer.ts
+++ ngrx-shopping-list/src/app/store/reducer/shopping.reducer.ts
@@ -16,11 +16,51 @@ const intialState: ShoppingState = {
 
 export function ShoppingReducer(state: ShoppingState = intialState, action: ShoppingAction) {
   switch (action.type) {
+    case ShoppingActionTypes.LOAD_SHOPPING:
+    return {
+      ...state,
+      loading: false
+    };
+    case ShoppingActionTypes.LOAD_SHOPPING_SUCCESS:
+      return {
+        ...state,
+        list: action.payload,
+        loading: true
+      };
+    case ShoppingActionTypes.LOAD_SHOPPING_FAILURE:
+        return {
+          ...state,
+          error: action.payload,
+          loading: false
+        };
+    case ShoppingActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        loading: true
+      };
     case ShoppingActionTypes.ADD_ITEM_SUCCESS:
       return  {...state, list: action.payload, loading: false};
     // tslint:disable-next-line: no-switch-case-fall-through
+    case ShoppingActionTypes.ADD_ITEM_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+       };
+    case ShoppingActionTypes.DELETE_ITEM:
+      return {
+        ...state,
+        loading: true
+      };
     case ShoppingActionTypes.DELETE_ITEM_SUCCESS:
       return {...state, list: state.list.filter(item => item.id !== action.payload), loading: false };
+    case ShoppingActionTypes.DELETE_ITEM_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      };
+
     default:
       return state;
 
